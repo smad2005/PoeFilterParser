ee216982fa5a6c68ebb57871af43e09243cbb581
smad2005 <smad2005@gmail.com>
2016-12-15 00:31:48 +0200

    Updated version of ANTLR

---
+++ Tests/PoeFilterParser.NUnit.IntegrationTests/FakePoeFilterVisitor.cs
@@ -0,0 +1,9 @@
+﻿using PoeFilterParser.Model;
+
+namespace PoeFilterParser.NUnit.IntegrationTests
+{
+	public class FakePoeFilterVisitor : PoeFilterBaseVisitor<bool>
+	{
+		public override bool VisitMain(Model.PoeFilterParser.MainContext context) => true;
+	}
+}
---
+++ Tests/PoeFilterParser.NUnit.IntegrationTests/PoeFilterVisitorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using Antlr4.Runtime;
+using NUnit.Framework;
+using PoeFilterParser.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PoeFilterParser.NUnit.IntegrationTests
+{
+	[TestFixture]
+	public class PoeFilterVisitorTests
+	{
+		[TestCaseSource(nameof(FiltersList))]
+		public void Validate(string path)
+		{
+			//var path = "X:\\!POE\\PoeFilterParser\\Tests\\PoeFilterParser.NUnit.IntegrationTests\\Filters\\Full.filter";
+			using (var fileStream = new StreamReader(path))
+			{
+				var input = new AntlrInputStream(fileStream);
+				var lexer = new PoeFilterLexer(input);
+				var tokens = new CommonTokenStream(lexer);
+				var parser = new Model.PoeFilterParser(tokens);
+				parser.RemoveErrorListeners();
+				parser.AddErrorListener(new ErrorListener());
+				var tree = parser.main();
+				var visitor = new FakePoeFilterVisitor();
+
+				var result = visitor.Visit(tree);
+
+				Assert.IsTrue(result);
+			}
+		}
+
+		public static IEnumerable<TestCaseData> FiltersList()
+		{
+			var folder = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)), "filters");
+			return Directory.GetFiles(folder).Select(f=>new TestCaseData(f) {TestName = $"Validate_{Path.GetFileNameWithoutExtension(f)}_Success"});
+		}
+
+	}
+}
